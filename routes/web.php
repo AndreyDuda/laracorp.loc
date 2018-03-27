dd24e9902a76ece4b3db3dd4318d0f1ecaa824e0
Duda <andreyduda88@gmail.com>
2018-03-27 22:16:57 +0300

    create index route for Indexcontroller --resource create mane SiteController and layouts

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');*/
+
+Route::resource('/', 'IndexController', ['only'  => ['index'],
+                                         'names' => ['index' => 'home']]);
